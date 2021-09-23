a565afbc86d899728f7995b1390f5308c791cc3f
Dawood Ahmed <DaudAhmed27786@>
2021-09-23 23:03:02 +0500

    improved font size

--- pages/Home_Page_Comps/hero_section.js
+++ pages/Home_Page_Comps/hero_section.js
@@ -14,7 +14,7 @@ const hero_section = () => {
   <div className="hero-container"> 
   <div className="hero-section "><div className="hero-main">
                <div className="hero-content-left">
-               <p className="">Your wish is <br/> our command.</p>
+               <p className="testings">Your wish is <br/> our command.</p>
                <div className="hero-left-main-content">
                <h1 class="-home">Start your switch <span className="">with Broadband Genie</span> </h1>
 
@@ -22,16 +22,19 @@ const hero_section = () => {
                <div class="pc-checker">
     <form action="#" >
      <div className='hero-form-container'>
-<input  type="text" className="hero-text-input" required="true"/>
-<label className="hero-hint-input">Enter Your Postcode</label>
+{/* <input  type="text" className="hero-text-input" required="true"/> */}
+{/* <label className="hero-hint-input">Enter Your Postcode</label> */}
 {/* <TextField id="standard-basic" className="hero-text-input mt-5" label="Standard" /> */}
-
+<div class="form-floating mb-3"> 
+  <input type="text" class="form-control hero-text-input" id="floatingInput" placeholder="&nbsp"/>
+  <label for="floatingInput" className="hero-hint-input">Enter Your Postcode</label>
+</div>
 <a href="#" id="postcode-submit">Start</a>
 </div>
 </form>
 </div>
 
-<a href="#" className="hero-why-postcode-link">Why do we need your code?</a>
+<a href="#" className="hero-why-postcode-link">Why do we need your postcode?</a>
 </div>
                </div> 
                <div className="hero-content-right">
--- pages/Home_Page_Comps/landpage_3.js
+++ pages/Home_Page_Comps/landpage_3.js
@@ -7,33 +7,31 @@ const [accorData ,setaccorData] = useState([
     {heading:"Why should I  use Broadband Genie?",
     // detail:"Broadband Genie is a free, simple to use and completely independent comparison service that will help you save money on home broadband, mobile broadband,mobile, tablets and TV."},{detail1:"  Our team of experts are constantly on the hunt for the latest and greatest offers. Our comparison tables have been designed to make it effortless for you to search and buy the ideal package. And our plain-English guides and features will cut through the jargon to troubleshoot problems and explain everything you need to know about buying and using broadband.",
     //  collapseId:"#collapseOne",collapse:"collapseOne",headingId:"HeadingOne"},
-    para1:"Broadband Genie is a free, simple to use and completely independent comparison service that will help you save money on home broadband,  mobile broadband,mobile, tablets and TV.",para2:"Our team of experts are constantly on the hunt for the latest and greatest offers. Our comparison tables have been designed to make it effortless for you to search and buy the ideal package. And our plain-English guides and features will cut through the jargon to troubleshoot problems and explain everything you need to know about buying and using broadband.",collapseId:"#collapseOne",collapse:"collapseOne",headingId:"HeadingOne"},
+    para1:"Broadband Genie is a free, simple to use and completely independent comparison service that will help you save money on home broadband,  mobile broadband,mobile, tablets and TV.",para2:"Our team of experts are constantly on the hunt for the latest and greatest offers. Our comparison tables have been designed to make it effortless for you to search and buy the ideal package. And our plain-English guides and features will cut through the jargon to troubleshoot problems and explain everything you need to know about buying and using broadband.",collapseId:"#panelsStayOpen-collapseOne",collapse:"panelsStayOpen-collapseOne",headingId:"panelsStayOpen-headingOne"},
     {heading:"How does Broadband Genie make money?",
     para1:"There is no charge for using Broadband Genie. We earn money from advertising and sales commissions, so when you find a great new deal and buy through us you're helping to support our services at no cost to yourself.",para2:"",
-     collapseId:"#collapseTwo",collapse:"collapseTwo",headingId:"HeadingTwo"},
+     collapseId:"#panelsStayOpen-collapseTwo",collapse:"panelsStayOpen-collapseTwo",headingId:"panelsStayOpen-headingTwo"},
     {heading:"Is Broadband Genie affiliated with any providers?",
     para1:"No. Broadband Genie is 100% independent and unbiased. Editorial content is free of interference and our comparison tools are designed, built and maintained by our own experts.",para2:"We work with all the providers listed on our comparison tables to ensure the information is up to date, and to bring you the latest offers. Advertising and sponsored deals appearing on Broadband Genie will always be clearly marked.",
-     collapseId:"#collapseThree",collapse:"collapseThree",headingId:"HeadingThree"},
+     collapseId:"#panelsStayOpen-collapseThree",collapse:"panelsStayOpen-collapseThree",headingId:"panelsStayOpen-headingThree"},
     {heading:"Will I save money by using Broadband Genie?",
     para1:"Yes. Our service has been designed to help you find the ideal broadband service with minimum effort. And we're constantly refining the comparison tables and site features to make it as easy as possible to compare the latest offers and save money.",para2:"We also work with providers to offer exclusive special offers and save even more. You won't find these deals on any other site.",
-     collapseId:"#collapseFour",collapse:"collapseFour",headingId:"HeadingFour"},
+     collapseId:"#panelsStayOpen-collapseFour",collapse:"panelsStayOpen-collapseFour",headingId:"panelsStayOpen-headingFour"},
     {heading:"Do you list every broadband provider?",
     para1:"You'll find all the major broadband ISPs which cover 95% of the market, alongside smaller providers offering specialist services. So while we don't list every single broadband ISP you will be able to find something that suits your needs at the right price.",para2:"",
-     collapseId:"#collapseFive",collapse:"collapseFive",headingId:"HeadingFive"},
+     collapseId:"#panelsStayOpen-collapseFive",collapse:"panelsStayOpen-collapseFive",headingId:"panelsStayOpen-headingFive"},
     {heading:"How do you decide what order to list products in the comparison table?",
     para1:"By default the comparison tables are sorted by popularity, which is a combination of number of clicks and sales. Sponsored deals are not included in the popularity rating.",para2:"You can change the order of products instantly using the sorting options or by clicking any of the table headings.",
- collapseId:"#collapseSix",collapse:"collapseSix",headingId:"HeadingSix"},]
+ collapseId:"#panelsStayOpen-collapseSix",collapse:"panelsStayOpen-collapseSix",headingId:"panelsStayOpen-headingSix"},]
     )
 
-    const [plus, setplus] = useState(false)
+    const [expandorNo, setexpandorNo] = useState(null)
     // const [exp, setexp] = useState(0)
     
-    const test =(id)=>{
-    setplus(!plus);
-    // setexp(id);
-    // console.log(plus)
-
-  
+    const expander =(id)=>{
+      setexpandorNo(String(id));
+      
+  console.log("Expandor----",id)
     }
 
     
@@ -69,23 +67,22 @@ const [accorData ,setaccorData] = useState([
         </div>
 
         
-        <div className="accordion" id="accordionExample">
+        <div className="accordion" id="accordionPanelsStayOpenExample">
     {accorData.map((value,index) => {
      return (<>
 
-  <div className="accordion-item">
+  <div className={`accordion-item `} onClick={()=>expander(index)}>
 
     <h2 className="accordion-header" id={value.headingId}   >
-      <button className="accordion-button collapsed" type="input" data-bs-toggle="collapse"
-     aria-expanded="false" onClick={()=>test(index)}>
+      <button className="accordion-button collapsed" type="input" data-bs-toggle="collapse"  data-bs-target={value.collapseId} aria-expanded="true"  aria-controls={value.collapse}  >
 
-       {/* data-bs-target={value.collapseId} aria-controls={value.collapse} aria-expanded="false" > */}
+      {/* aria-expanded="false" > */}
       {value.heading}
-      <div className="accordion-right-btn"><div className="expandor expandor1"></div>
-    <div className={`expandor expandor2`}></div></div>
+      <div className="accordion-right-btn"><div className="expandor expandor_one"></div>
+    <div className={`expandor expandor${String(index)} `}></div></div>
       </button>
     </h2>
-    <div id={value.collapse} className="accordion-collapse collapse" aria-labelledby={value.headingId} data-bs-parent="#accordionExample">
+    <div id={value.collapse} className="accordion-collapse collapse" aria-labelledby={value.headingId} >
       <div className="accordion-body">
         <span  className="para1 d-inline-block">{value.para1} </span>
          <span className="para2 d-inline-block">{value.para2} </span>
@@ -100,7 +97,7 @@ const [accorData ,setaccorData] = useState([
      
      })}
 
-
+ 
 
  </div>
     </div>
@@ -112,14 +109,14 @@ const [accorData ,setaccorData] = useState([
 
 </div>
  <style global jsx>{`
-  
 
-  .expandor2{
-   transform: ${plus ? "rotateX(90deg)":"rotateX(0deg)"} ;
+  .expandor${expandorNo}{
+   transform: rotateX(90deg);
    transition: 1s;
 }
 
  `}</style> 
+   {/* transform: ${plus ? "rotateX(90deg)":"rotateX(0deg)"} ; */}
         
         </>
     )
